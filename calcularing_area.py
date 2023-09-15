c3bad74c1f7b3297f109422daa5b7df11b2f69a5
Michael <thesuspect182@gmail.com>
2023-09-15 19:59:56 +0300

    added the usage of abstract classes

--- calcularing_area.py
+++ calcularing_area.py
@@ -1,37 +1,38 @@
 # 15.09.2023 Mikhail Porokhnya
 
+from abc import ABC, abstractmethod
 import math
 
 
-class CalculatingGeometryArea:
-    @staticmethod
-    def triangle_area(side1, side2, side3):
-        """
-        Calculates the area of a triangle based on three sides using Heron's formula.
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
 
-        :param side1: First side length.
-        :param side2: Second side length.
-        :param side3: Third side length.
-        :return: Square of a triangle.
-        """
-        if side1 <= 0 or side2 <= 0 or side3 <= 0:
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        if self.radius < 0:
+            raise ValueError("radius cannot be negative")
+        return math.pi * self.radius ** 2
+
+
+class Triangle(Shape):
+    def __init__(self, side1, side2, side3):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+
+    def calculate_area(self):
+        if self.side1 <= 0 or self.side2 <= 0 or self.side3 <= 0:
             raise ValueError("Side lengths must be positive numbers")
 
         # Semi-perimeter of a triangle
-        s = (side1 + side2 + side3) / 2
+        s = (self.side1 + self.side2 + self.side3) / 2
 
         # Heron's formula for calculating square
-        area = (s * (s - side1) * (s - side2) * (s - side3)) ** 0.5
+        area = (s * (s - self.side1) * (s - self.side2) * (s - self.side3)) ** 0.5
         return area
-
-    @staticmethod
-    def circle_area(radius):
-        """
-        Calculates the area of a circle by radius
-
-        :param radius: Circle radius.
-        :return: Square of a circle.
-        """
-        if radius < 0:
-            raise ValueError("Radius cannot be negative")
-        return math.pi * radius ** 2
